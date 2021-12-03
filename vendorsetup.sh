85b4637ff3162673aa686b4a4243ddc89def2102
zaidkhan0997 <kzaid0997@gmail.com>
2021-12-03 19:16:07 +0530

    sweet: update vendorsetup.sh
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,2 +1,9 @@
+export RELAX_USES_LIBRARY_CHECK=true
+
+# Nuke
+rm -rf hardware/qcom-caf/common
+
 # clone
 git clone -b 12 https://github.com/zaidkhan0997/vendor_GoogleCameraSweet.git vendor/GoogleCameraSweet
+git clone -b arrow-12.0 https://github.com/ArrowOS/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
+git clone -b arrow-12.0 https://github.com/ArrowOS/android_vendor_qcom_opensource_display-commonsys.git vendor/qcom/opensource/commonsys/display
