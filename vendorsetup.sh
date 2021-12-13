1243ff86634097ddd352034ed57cd062d28837cf
zaidkhan0997 <kzaid0997@gmail.com>
2021-12-13 16:01:22 +0530

    sweet: switch to ventom kernel
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,9 +1,11 @@
-export RELAX_USES_LIBRARY_CHECK=true
+#export RELAX_USES_LIBRARY_CHECK=true
 
 # Nuke
 rm -rf hardware/qcom-caf/common
 
 # clone
-git clone -b 12 https://github.com/zaidkhan0997/vendor_GoogleCameraSweet.git vendor/GoogleCameraSweet
+#git clone -b 12 https://github.com/zaidkhan0997/vendor_GoogleCameraSweet.git vendor/GoogleCameraSweet
 git clone -b arrow-12.0 https://github.com/ArrowOS/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
-git clone -b arrow-12.0 https://github.com/ArrowOS/android_vendor_qcom_opensource_display-commonsys.git vendor/qcom/opensource/commonsys/display
+#git clone -b arrow-12.0 https://github.com/ArrowOS/android_vendor_qcom_opensource_display-commonsys.git vendor/qcom/opensource/commonsys/display
+git clone -b master --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+git clone -b courbet --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet 
