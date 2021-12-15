eee933fb879202a7d891b0f139df93909a03baf4
zaidkhan0997 <kzaid0997@gmail.com>
2021-12-16 00:39:03 +0530

    sweet: track gcam from my gitlab
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,7 +4,7 @@
 rm -rf hardware/qcom-caf/common
 
 # clone
-#git clone -b 12 https://github.com/zaidkhan0997/vendor_GoogleCameraSweet.git vendor/GoogleCameraSweet
+git clone -b 12 --depth=1 https://gitlab.com/zaidkhan0997/vendor_GoogleCameraSweet.git vendor/GoogleCameraSweet
 git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
 #git clone -b arrow-12.0 https://github.com/ArrowOS/android_vendor_qcom_opensource_display-commonsys.git vendor/qcom/opensource/commonsys/display
 git clone -b master --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
