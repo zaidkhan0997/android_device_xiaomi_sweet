375249d811e58c2ffceb75caa1e85fc9dac4f0ea
zaidkhan0997 <kzaid0997@gmail.com>
2022-01-09 14:17:17 +0530

    sweet: update vendorsetup.sh
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -1,11 +1,9 @@
-#export RELAX_USES_LIBRARY_CHECK=true
-
 # Nuke
 rm -rf hardware/qcom-caf/common
+rm -rf device/qcom/sepolicy_vndr
+rm -rf hardware/qcom-caf/sm8150/display
 
 # clone
-#git clone -b 12 --depth=1 https://gitlab.com/zaidkhan0997/vendor_GoogleCameraSweet.git vendor/GoogleCameraSweet
+git clone -b arrow-12.0-caf-sm8150 https://github.com/ArrowOS/android_hardware_qcom_display.git hardware/qcom-caf/sm8150/display
+git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_device_qcom_sepolicy_vndr-1.git device/qcom/sepolicy_vndr
 git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
-#git clone -b arrow-12.0 https://github.com/ArrowOS/android_vendor_qcom_opensource_display-commonsys.git vendor/qcom/opensource/commonsys/display
-git clone -b master --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
-git clone -b eleven --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
