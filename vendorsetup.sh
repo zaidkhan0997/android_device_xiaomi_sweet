561f989161f549ccf6afabbab705c192ae80a4fe
zaidkhan0997 <kzaid0997@gmail.com>
2022-01-26 14:59:48 +0530

    sweet: track ventom kernel and proton clang
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -8,3 +8,5 @@ git clone -b twelve https://github.com/zaidkhan0997/hardware_qcom-caf_sm8150-com
 git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_device_qcom_sepolicy_vndr-1.git device/qcom/sepolicy_vndr
 git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
 git clone -b twelve https://gitlab.com/sweetin/android-vendor-dolby.git vendor/dolby
+git clone -b master --depth=1 https://github.com/kdrag0n/proton-clang.git prebuilts/clang/host/linux-x86/clang-proton
+git clone -b courbet --depth=1 https://github.com/vantoman/kernel_xiaomi_sm6150.git kernel/xiaomi/sweet
