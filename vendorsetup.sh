b1714611da7b896c96addd6b42fc266f7af1384f
zaidkhan0997 <kzaid0997@gmail.com>
2022-01-26 14:50:34 +0530

    sweet: track custom display hal
    Signed-off-by: zaidkhan0997 <kzaid0997@gmail.com>

--- vendorsetup.sh
+++ vendorsetup.sh
@@ -4,6 +4,6 @@ rm -rf device/qcom/sepolicy_vndr
 rm -rf hardware/qcom-caf/sm8150/display
 
 # clone
-git clone -b arrow-12.0-caf-sm8150 https://github.com/ArrowOS/android_hardware_qcom_display.git hardware/qcom-caf/sm8150/display
+git clone -b twelve https://github.com/zaidkhan0997/hardware_qcom-caf_sm8150-common_display.git hardware/qcom-caf/sm8150/display
 git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_device_qcom_sepolicy_vndr-1.git device/qcom/sepolicy_vndr
 git clone -b arrow-12.0 https://github.com/zaidkhan0997/android_hardware_qcom-caf_common.git hardware/qcom-caf/common
